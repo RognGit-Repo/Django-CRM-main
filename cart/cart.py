7fbb065bf90d514be4e0b04081179c630bb50c8a
RognGit-Repo <ronald.reyes.me@gmail.com>
2025-01-26 17:07:57 +0800

    cart added

---
+++ cart/cart.py
@@ -0,0 +1,34 @@
+class Cart():
+    def __init__(self, request):
+        self.session=request.session
+
+        cart=self.session.get('session_key')
+
+        if 'session_key' not in request.session:
+            cart=self.session['session_key']={}
+         
+        
+        self.cart=cart
+
+    def add(self, product):
+        product_id=str(product.id)
+
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id]={'name':product.name,'price': str(product.price)}
+        
+        self.session.modified=True
+
+    def __len__(self):
+        return len(self.cart)
+    
+
+    def get_cart(self):
+        return list(self.cart.values())
+    
+
+
+
+
+
--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,23 @@
-from django.shortcuts import render, redirect
-
+from django.shortcuts import render, redirect,get_object_or_404
+from .cart import Cart
+from website.models import Product
+from django.http import JsonResponse
+import json
 # Create your views here.
 def cart_add(request):
-    pass
+    cart=Cart(request)
+
+    if request.POST.get('action')=='post':
+        product_id=int(request.POST.get('product_id'))
+
+        product=get_object_or_404(Product, id=product_id)
+
+        cart.add(product=product)
+
+        cart_qty=cart.__len__()
+        response=JsonResponse({'qty': cart_qty, 'cart':json.dumps(cart.get_cart())})
+
+        return response
 
 def cart_delete(request):
     pass
